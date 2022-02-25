c27d32478b654c48e2c8903f85d966e223feb01a
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-02-25 11:15:59 +0700

    fix: update number 4

--- soal-4.js
+++ soal-4.js
@@ -22,7 +22,7 @@ const isValidPassword = (givenPassword) => {
 
 console.log(isValidPassword('Meong2021'));
 console.log(isValidPassword('meong2021'));
-console.log(isValidPassword('@eong2021'));
+console.log(isValidPassword('@eong'));
 console.log(isValidPassword('Meong2'));
 console.log(isValidPassword(0));
 console.log(isValidPassword());
