601802cf878a5bc6a1a74c96aaf8cd869df5bbd9
unknown <shobrinaf@gmail.com>
2023-01-04 10:22:55 +0700

    test

--- challenge-01/soal-6.js
+++ challenge-01/soal-6.js
@@ -13,8 +13,8 @@ const getAngkaTerbesarKedua = (dataNumber) => {
 };
 
 const dataAngka = [9, 4, 7, 7, 4, 3, 2, 2, 8];
-// console.log(getAngkaTerbesarKedua(dataAngka));
-// console.log(getAngkaTerbesarKedua(0));
+console.log(getAngkaTerbesarKedua(dataAngka));
+console.log(getAngkaTerbesarKedua(0));
 // console.log(getAngkaTerbesarKedua());
 
 module.exports = getAngkaTerbesarKedua;
